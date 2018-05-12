3930fe5e15507c2e7483fed746d628953313949f
須藤悠太 <yuta.sudo.gis@gmail.com>
2018-05-12 14:39:39 +0900

    レイアウトの変更、INSERTともできますね

--- post.php
+++ post.php
@@ -15,10 +15,12 @@ if(!empty($_POST)){
 	$error['image']  = 'type';
 	}
 
-	$book_sql = 'INSERT INTO `book_recommend` SET  `bookname`, `member_id`, `reason`, `book_pic`, `created` = NOW() ';
-	$book_data = array($_POST['bookname'],$_SELLECT['member_id'],$_POST['reason'],$_POST['book_pic']);
+	$book_sql = 'INSERT INTO `book_recommends` SET  `bookname`=?, `member_id`=?, `reason`=?, `book_pic`=?, `created` = NOW()';
+	$book_data = array($_POST['bookname'],$_SESSION['id'],$_POST['reason'],$book_pic);
 	$stmt = $dbh->prepare($book_sql);
 	$stmt->execute($book_data);
+	header('Location: home.php');
+	exit;
 }
  ?>
 <!DOCTYPE HTML>
@@ -75,7 +77,9 @@ if(!empty($_POST)){
 	<!--[if lt IE 9]>
 	<script src="js/respond.min.js"></script>
 	<![endif]-->
-
+	<style>#gtco-header{ margin: 0px; background-image: url("assets/images/p0521_l.png")}
+#page{margin: 0px ;padding-top: 40px; ;
+}</style>
 	</head>
 	<body>
 
@@ -86,12 +90,12 @@ if(!empty($_POST)){
 			<div class="gtco-container">
 				<div class="row">
 					<div class="col-sm-4 col-xs-12">
-						<div><a href="index.html"><h1 style="font-size: 30px; color: #a9a9a9; margin: auto; ">投稿する</h1></a></div>
+						<div><a href="index.html"><h1 style="font-size: 30px; color: #a9a9a9; margin: auto; ">Reccomend your book</h1></a></div>
 					</div>
 					<div class="col-xs-8 text-right menu-1">
 						<ul>
-							<li class="active"><a href="home.php">Home</a></li>
-							<li><a href="post.php">投稿する</a></li>
+							<li><a href="home.php">Home</a></li>
+							<li class="active"><a href="post.php">投稿する</a></li>
 							<li><a href="profile.php">マイページ</a></li>
 							<li><a href="logout.php">ログアウト</a></li>
 						</ul>
@@ -101,50 +105,81 @@ if(!empty($_POST)){
 		</nav>
 
 		<header id="gtco-header" class="gtco-cover" role="banner">
-			<style>#gtco-header{margin-top:-53px;   background-image: url("assets/images/p0521_l.png")}</style>
 			<div class="gtco-container">
 				<div class="row header-img">
 					<div class="col-md-1 col-xs-0"></div>
-					<div class="col-md-10 col-md-offset-0 text-left">
-						<div class="display-t">
-							<div class="display-tc">
-								<div class="row header-form">
-									<h1 style="text-align:center; font-family: 'Gulim' ,sans-serif ;">おすすめの本を共有しよう</h1>
+						<div class="col-md-10text-left">
+							<div class="display-t">
+								<div class="display-tc">
 									<form method="post" class="form-horizontal" role="form" enctype="multipart/form-data">
-											<div class="login">
+										<div class="header-form">
+											<div class="row">
+												<div class="animate-box">
+													<h1 style="text-align:center; font-family: 'Gulim' ,sans-serif ;">
+													おすすめの本を共有しよう
+													</h1>
+												</div>
+												<div class="login">
 													<div class="col-md-6 copy animate-box" >
 													<!-- 本の名前 -->
-														<div class="form-group" style="padding:0px 30px;">
+														<div class="form-group" style="padding:  0px 30px;">
 															<label class="control-label">本の名前</label>
 															<input type="text" name="bookname" class="form-control" placeholder="例： 吾輩は猫である">
 														</div>
-													<!-- パスワード -->
-														<div class="form-group" style="padding:0px 30px;">
-															<label class=" control-label">本の写真<span style="font-size: 12px;">(jpg、png、gifにて)</span></label>
-															<input type="file" name="book_pic" class="form-control">
-															<?php if (isset($error['image']) && $error['image'] == 'type') { ?>
-															<p class="error">* jpg、png、gifのいずれかの拡張子を選んでください。</p>
-															<?php } ?>
-														</div>
-														</div>
-														<div class="col-md-6 copy animate-box" >
-													<!-- 本の名前 -->
+													<!-- 本の理由 -->
 														<div class="form-group" style="padding:0px 30px;">
 															<label class="control-label">おすすめする理由</label>
-															<textarea rows="6" style="resize:vertical; background: rgba(0, 0, 0, 0.1);" name= "reason" class="full-width form-control" placeholder="おすすめポイント！" ></textarea>
+															<textarea rows="9" style="resize:vertical; background: rgba(0, 0, 0, 0.1);" name= "reason" class="full-width form-control" placeholder="おすすめポイント！" ></textarea>
 														</div>
-														</div>
-
 													</div>
-												</form>
+													<div class="col-md-6 copy animate-box" >
+														<div class="form-group" style="padding:0px 30px 30px 30px;">
+															<label class=" control-label">本の写真<span style="font-size: 12px;">(jpg、png、gifにて)</span></label>
+															<input id="selfile" type="file" name="book_pic" class="form-control">
+															<?php if (isset($error['image']) && $error['image'] == 'type') { ?>
+															<p class="error">* jpg、png、gifのいずれかの拡張子を選んでください。</p>
+																	<?php } ?>
+															<div id="bg" style="margin-left: 50px; width: 300px; height:300px;">
+																<p id="wait" style="padding-top:138px; text-align: center">ここに写真を表示します</p>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="row">
+												<div class="col-md-4 col-xs-2"></div>
+													<div class="col-md-4 col-xs-8">
+														<button class="top-btn" type='submit' class="button button-primary " style="width: 100%; margin: 25px 0px;" >投稿する</button>
+													</div>
+												<div class="col-md-4 col-xs-2"></div>
+											</div>
 										</div>
-									</div>
+									</form>
 								</div>
 							<div class="col-md-1 col-xs-0"></div>
 						</div>
 					</div>
 				</div>
 		</header>
+<script>
+var pre = document.getElementById("selfile");
+
+
+pre.addEventListener("change", function(evt){
+document.getElementById("wait").innerHTML = "少々お待ちください";
+  var file = evt.target.files;
+  var reader = new FileReader();
+  //dataURL形式でファイルを読み込む
+  reader.readAsDataURL(file[0]);
+  //ファイルの読込が終了した時の処理
+  reader.onload = function(){
+    var dataUrl = reader.result;
+    //読み込んだ画像とdataURLを書き出す
+    document.getElementById("bg").innerHTML = "<img src='" + dataUrl + "' width='300' height='300' >";
+    document.getElementById("dturl").value = dataUrl;
+  }
+},false);
+</script>
 		<!-- END #gtco-header -->
 	<!-- jQuery -->
 	<script src="assets/js/jquery.min.js"></script>
