3b7aaef9db0bf0d7c98c36a2802101bc0f29f44a
須藤悠太 <yuta.sudo.gis@gmail.com>
2018-05-12 14:41:08 +0900

    レイアウトの変更

--- register/register.php
+++ register/register.php
@@ -115,34 +115,12 @@ if (!empty($_POST)) {
 						<div><a href="../index.php"><h1 style="font-size: 30px; color: #a9a9a9; margin: auto; ">新規登録</h1></a></div>
 					</div>
 					<div class="col-xs-8 text-right menu-1">
-
-<!-- ログイン、登録、確認画面ではコメントアウト// -->
-					<!-- 	<ul>
-							<li class="active"><a href="index.html">Home</a></li>
-							<li><a href="about.html">About</a></li>
-							<li class="has-dropdown">
-								<a href="services.html">Services</a>
-								<ul class="dropdown">
-									<li><a href="#">Web Design</a></li>
-									<li><a href="#">eCommerce</a></li>
-									<li><a href="#">Branding</a></li>
-									<li><a href="#">API</a></li>
-								</ul>
-							</li>
-							<li class="has-dropdown">
-								<a href="#">Dropdown</a>
-								<ul class="dropdown">
-									<li><a href="#">HTML5</a></li>
-									<li><a href="#">CSS3</a></li>
-									<li><a href="#">Sass</a></li>
-									<li><a href="#">jQuery</a></li>
-								</ul>
-							</li>
-							<li><a href="portfolio.html">Portfolio</a></li>
-							<li><a href="contact.html">Contact</a></li>
-						</ul> -->
-<!-- // -->
-
+						<div clas="row">
+							<ul>
+								<li class="col-xs-6 text-center menu-1"><p style="color: #fff; text-decoration: underline;">投稿する</p></li>
+								<li class="col-xs-6 text-center menu-1"><p>マイページ</p></li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
