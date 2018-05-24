6f47b54d9dab16747a9cc33bad6ed825e8f7b710
須藤悠太 <yuta.sudo.gis@gmail.com>
2018-05-25 00:12:37 +0900

    SQL文の訂正

--- edit.php
+++ edit.php
@@ -1,15 +1,22 @@
 <?php 
 require('dbconnect.php');
 session_start();
-// require('func.php');
-// login_check();
+require('func.php');
+login_check();
 
 	$recommend_sql='SELECT * FROM `book_recommends` WHERE `recommend_id` = ? ';
 	$recommend_data= array($_GET['id']);
 	$stmt = $dbh->prepare($recommend_sql);
 	$stmt->execute($recommend_data);
 	$recommend =$stmt -> fetch(PDO::FETCH_ASSOC);
-
+echo('<br>'); 
+echo('<br>');
+echo('<pre>');
+var_dump($recommend) ;
+echo('</pre>');
+//echo('<pre>');
+//var_dump($hoge) ;
+//echo('</pre>');
 if(!empty($_POST)){
 	$type = substr($_FILES['book_pic']['name'], -3);
 	$type = strtolower($type);
@@ -20,9 +27,11 @@ if(!empty($_POST)){
 	}else{
 	$error['image']  = 'type';
 	}
+	$bookname = htmlspecialchars($_POST['bookname']);
+	$reason = htmlspecialchars($_POST['reason']);
 
-	$book_sql = 'UPDATE `book_recommend` SET  `bookname`, `member_id`, `reason`, `book_pic`, `created` = NOW() ';
-	$book_data = array($_POST['bookname'],$_SELLECT['member_id'],$_POST['reason'],$_POST['book_pic']);
+	$book_sql = 'UPDATE `book_recommends` SET `bookname` = ?, `member_id` = ?, `reason` = ?, `book_pic` = ?, `modified` = NOW() WHERE `recommend_id` =? ';
+	$book_data = array($bookname,$_SESSION['id'],$reason,$book_pic,$_GET['id']);
 	$stmt = $dbh->prepare($book_sql);
 	$stmt->execute($book_data);
 }
@@ -37,7 +46,7 @@ if(!empty($_POST)){
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>新規投稿</title>
+	<title>投稿編集</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
 	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
@@ -81,7 +90,7 @@ if(!empty($_POST)){
 	<!--[if lt IE 9]>
 	<script src="js/respond.min.js"></script>
 	<![endif]-->
-
+	<style>#gtco-header{  background-image: url("assets/images/p0521_l.png") } </style>
 	</head>
 	<body>
 
@@ -92,11 +101,11 @@ if(!empty($_POST)){
 			<div class="gtco-container">
 				<div class="row">
 					<div class="col-sm-4 col-xs-12">
-						<div><a href="index.html"><h1 style="font-size: 30px; color: #a9a9a9; margin: auto; ">投稿する</h1></a></div>
+						<div><a href="index.html"><h1 style="font-size: 30px; color: #a9a9a9; margin: auto; ">Reccomend your book</h1></a></div>
 					</div>
 					<div class="col-xs-8 text-right menu-1">
 						<ul>
-							<li class="active"><a href="home.php">Home</a></li>
+							<li><a href="home.php">Home</a></li>
 							<li><a href="post.php">投稿する</a></li>
 							<li><a href="profile.php">マイページ</a></li>
 							<li><a href="logout.php">ログアウト</a></li>
@@ -107,50 +116,82 @@ if(!empty($_POST)){
 		</nav>
 
 		<header id="gtco-header" class="gtco-cover" role="banner">
-			<style>#gtco-header{margin-top:-53px;   background-image: url("assets/images/p0521_l.png")}</style>
 			<div class="gtco-container">
 				<div class="row header-img">
 					<div class="col-md-1 col-xs-0"></div>
-					<div class="col-md-10 col-md-offset-0 text-left">
-						<div class="display-t">
-							<div class="display-tc">
-								<div class="row header-form">
-									<h1 style="text-align:center; font-family: 'Gulim' ,sans-serif ;">おすすめの本を共有しよう</h1>
+						<div class="col-md-10text-left">
+							<div class="display-t">
+								<div class="display-tc">
 									<form method="post" class="form-horizontal" role="form" enctype="multipart/form-data">
-											<div class="login">
+										<div class="header-form">
+											<div class="row">
+												<div class="animate-box">
+													<h1 style="text-align:center; font-family: 'Gulim' ,sans-serif ;">
+													投稿の編集
+													</h1>
+												</div>
+												<div class="login">
 													<div class="col-md-6 copy animate-box" >
 													<!-- 本の名前 -->
-														<div class="form-group" style="padding:0px 30px;">
+														<div class="form-group" style="padding:  0px 30px;">
 															<label class="control-label">本の名前</label>
-															<input type="text" name="bookname" class="form-control" value="<?php echo $recommend['bookname'] ?>">
+															<input type="text" name="bookname" class="form-control" value="<?php echo $recommend['bookname']?>">
 														</div>
-													<!-- パスワード -->
-														<div class="form-group" style="padding:0px 30px;">
-															<label class=" control-label">本の写真<span style="font-size: 12px;">(jpg、png、gifにて)</span></label>
-															<input type="file" name="book_pic" class="form-control" value="<?php echo $recommend['book_pic'] ?>>
-															<?php if (isset($error['image']) && $error['image'] == 'type') { ?>
-															<p class="error">* jpg、png、gifのいずれかの拡張子を選んでください。</p>
-															<?php } ?>
-														</div>
-														</div>
-														<div class="col-md-6 copy animate-box" >
-													<!-- おすすめする理由 -->
+													<!-- 本の理由 -->
 														<div class="form-group" style="padding:0px 30px;">
 															<label class="control-label">おすすめする理由</label>
-															<textarea rows="6" style="resize:vertical; background: rgba(0, 0, 0, 0.1);" name= "reason" class="full-width form-control" placeholder="おすすめポイント！" value="<?php echo $recommend['reason'] ?>" ></textarea>
+															<textarea rows="9" style="resize:vertical; background: rgba(0, 0, 0, 0.1);" name= "reason" class="full-width form-control"><?php echo $recommend['reason']?></textarea>
 														</div>
-														</div>
-
 													</div>
-												</form>
+													<div class="col-md-6 copy animate-box" >
+														<div class="form-group" style="padding:0px 30px 30px 30px;">
+															<label class=" control-label">本の写真<span style="font-size: 12px;">(jpg、png、gifにて)</span></label>
+															<input id="selfile" type="file" name="book_pic" class="form-control">
+															<?php if (isset($error['image']) && $error['image'] == 'type') { ?>
+															<p class="error">* jpg、png、gifのいずれかの拡張子を選んでください。</p>
+																	<?php } ?>
+															<div id="bg" style="margin-left: 0px; width: 300px; height:300px;">
+																<p>変更前</p>
+																<img id="wait" src="pic_reommend/<?php echo $recommend['book_pic'] ?>" width="300" height="300">
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="row">
+												<div class="col-md-4 col-xs-2"></div>
+													<div class="col-md-4 col-xs-8">
+														<button class="top-btn" type='submit' class="button button-primary " style="width: 100%; margin: 25px 0px;" >投稿する</button>
+													</div>
+												<div class="col-md-4 col-xs-2"></div>
+											</div>
 										</div>
-									</div>
+									</form>
 								</div>
 							<div class="col-md-1 col-xs-0"></div>
 						</div>
 					</div>
 				</div>
 		</header>
+<script>
+var pre = document.getElementById("selfile");
+
+
+pre.addEventListener("change", function(evt){
+document.getElementById("wait").innerHTML = "変更中";
+  var file = evt.target.files;
+  var reader = new FileReader();
+  //dataURL形式でファイルを読み込む
+  reader.readAsDataURL(file[0]);
+  //ファイルの読込が終了した時の処理
+  reader.onload = function(){
+    var dataUrl = reader.result;
+    //読み込んだ画像とdataURLを書き出す
+    document.getElementById("bg").innerHTML = "<p>変更後</p><img src='" + dataUrl + "' width='300' height='300' >";
+    document.getElementById("dturl").value = dataUrl;
+  }
+},false);
+</script>
 		<!-- END #gtco-header -->
 	<!-- jQuery -->
 	<script src="assets/js/jquery.min.js"></script>
