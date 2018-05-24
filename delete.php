0d518ce47c301c02e59a75c8556b111132d520bf
須藤悠太 <yuta.sudo.gis@gmail.com>
2018-05-25 00:09:24 +0900

    SQL文の訂正

--- delete.php
+++ delete.php
@@ -1,10 +1,12 @@
- <?php 
- require('function.php');
- login_check();
-  $sql = 'UPDATE `book_recommends` SET `	book_del_flg` = 1  WHERE `recommend_id`=?';
-  $delete = array($_GET['member_id']);
-  $stmt = $dbh->prepare($sql);
-  $stmt->execute($delete); 
-  header('Location: index.php');
-  exit();}
-   ?>
+ <?php
+	require('dbconnect.php');
+	session_start();
+	require('func.php');
+	login_check();
+	$sql = 'UPDATE `book_recommends` SET `book_del_flg` = 1  WHERE `recommend_id`=?';
+	$delete = array($_GET['id']);
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute($delete); 
+	header('Location: home.php');
+	exit();
+?>
