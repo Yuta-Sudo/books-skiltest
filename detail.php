190c4f6661a381fe090599c0a25d4f40f28dbbab
須藤悠太 <yuta.sudo.gis@gmail.com>
2018-05-25 21:40:25 +0900

    機能済み[レイアウトの変更]

--- detail.php
+++ detail.php
@@ -70,25 +70,25 @@ login_check();
 	</head>
 	<body>
 
-	<div class="gtco-loader"></div>
+<div class="gtco-loader"></div>
 	<div id="page">
-		<nav class="gtco-nav" role="navigation">
-			<div class="gtco-container" ">
-				<div class="row">
-					<div class="col-sm-4 col-xs-12">
-						<div><a href="index.html"><h1 style="font-size: 30px; color: #a9a9a9; margin: auto; ">Reccomend your book</h1></a></div>
-					</div>
-					<div class="col-xs-8 text-right menu-1">
-						<ul>
-							<li class="active"><a href="home.php">Home</a></li>
-							<li><a href="post.php">投稿する</a></li>
-							<li><a href="profile.php">マイページ</a></li>
-							<li><a href="logout.php">ログアウト</a></li>
-						</ul>
-					</div>
+	<nav class="gtco-nav" role="navigation">
+		<div class="gtco-container" >
+			<div class="row">
+				<div class="col-sm-4 col-xs-12">
+					<div><a href="home.php"><h1 style="font-size: 30px; color: #a9a9a9; margin: auto; ">Reccomend your book</h1></a></div>
+				</div>
+				<div class="col-xs-8 text-right menu-1">
+					<ul>
+						<li><a href="home.php">Home</a></li>
+						<li><a href="post.php">投稿する</a></li>
+						<li><a href="mypage.php">マイページ</a></li>
+						<li><a href="logout.php">ログアウト</a></li>
+					</ul>
 				</div>
 			</div>
-		</nav>
+		</div>
+	</nav>
 
 		<header id="gtco-header" class="gtco-cover" role="banner">
 			<style>#gtco-header{  background-image: url("assets/images/p0521_l.png") } </style>
@@ -99,28 +99,26 @@ login_check();
 						<div class="display-t">
 							<div class="display-tc">
 								<div class="row header-form" >
-									<?php if (isset($_SESSION['id']) && $recommend['member_id'] == $_SESSION['id']) : ?>
 									<div class="row" >
-										<div class="col-md-4"></div>
-										<div class="col-md-4">
-											<div class="form-group control-label" style=" padding:30px 30px;">
-												<a href="edit.php?id=<?php echo $recommend['recommend_id'] ?>"><button class="top-btn" type='submit' class="button button-primary" >編集する</button></a>
-											</div>
+										<div class="col-md-2"></div>
+										<div class="col-md-6">
+											<h1 style="text-align:center; font-family: 'Gulim' ,sans-serif ;">本の詳細</h1>
 										</div>
-										<div class="col-md-4">
-											<div class="form-group control-label" style=" padding:30px 30px;">
-												<a href="delete.php?id=<?php echo $recommend['recommend_id'] ?>"><button class="top-btn" type='submit' class="button button-primary" >削除する</button></a>
-											</div>
+										<div class="col-md-4" style="padding-top: 5px;">
+											<?php if (isset($_SESSION['id']) && $recommend['member_id'] == $_SESSION['id']) : ?>
+												<div class="form-group control-label">
+													<a href="edit.php?id=<?php echo $recommend['recommend_id'] ?>"><button class="top-btn" type='submit' class="button button-primary" >編集する</button></a>
+													<a href="delete.php?id=<?php echo $recommend['recommend_id'] ?>"><button class="top-btn" type='submit' class="button button-primary" >削除する</button></a>
+												</div>
+											<?php endif ?>
 										</div>
 									</div>
-									<?php endif ?>
-									<h1 style="text-align:center; font-family: 'Gulim' ,sans-serif ;">本の詳細</h1>
 									<form method="post" class="form-horizontal" role="form" enctype="multipart/form-data">
 											<div class="login">
 													<div class="col-md-6 copy animate-box" >
 													<!-- 本の名前 -->
 														<div class="form-group" style="padding:0px 30px;">
-															<label class="control-label">本の名前</label>
+															<label class="control-label">本のタイトル</label>
 															<div><p class="check" style="display:inline; height: auto;"><?php echo($recommend['bookname']); ?></p>
 														</div>
 													<!-- 写真 -->
@@ -136,8 +134,9 @@ login_check();
 													<!-- 本の名前 -->
 													<div class="form-group" style="padding:0px 30px; display:inline; height: auto;" >
 														<label class="control-label">おすすめする理由</label>
-														<div ><p class="check" style="display:inline; height: auto;"><?php echo($recommend['reason']); ?></p>
-													</div>
+														<div class="col-md-12 col-sm-12" style="height:250px; overflow:hidden; overflow-y:scroll; border:0px solid #ccc; padding:0 20px;">
+														<p class="check" style="color:#000; word-wrap: break-word; display: inline-block; width: 100%;"><?php echo $recommend['reason'] ?></p>
+														</div>
 													</div>
 												</div>
 											</form>
@@ -149,6 +148,7 @@ login_check();
 					</div>
 				</div>
 		</header>
+	</div>
 		<!-- END #gtco-header -->
 	<!-- jQuery -->
 	<script src="assets/js/jquery.min.js"></script>
